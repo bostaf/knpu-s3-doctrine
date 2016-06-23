4c40fbbd5be64c6f2b4d75729b443a989eb2c964
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2016-06-23 20:34:09 +0200

    Add links from list to show

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -43,12 +43,19 @@ class GenusController extends Controller
     }
 
     /**
-     * @Route("/genus/{genusName}")
+     * @Route("/genus/{genusName}", name="genus_show")
      */
     public function showAction($genusName)
     {
-        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+        $em = $this->getDoctrine()->getManager();
+        $genus = $em->getRepository('AppBundle:Genus')
+            ->findOneBy(['name' => $genusName]);
 
+        if ( ! $genus) {
+            throw $this->createNotFoundException('No genus found');
+        }
+
+        /*
         $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
         $key = md5($funFact);
         if ($cache->contains($key)) {
@@ -59,13 +66,13 @@ class GenusController extends Controller
                 ->transform($funFact);
             $cache->save($key, $funFact);
         }
+        */
 
         $this->get('logger')
             ->info('Showing genus: '.$genusName);
 
         return $this->render('genus/show.html.twig', array(
-            'name' => $genusName,
-            'funFact' => $funFact,
+            'genus' => $genus
         ));
     }
 
